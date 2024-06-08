1a9dec0ebade084c1cd3fa3ad267ab42bd7b06e0
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-08 15:50:35 +0300

    Finish 04 remove from array task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+    //creating new array without elements in theArgs array
+    let result = arr.filter(number => !theArgs.includes(number));
+
+    return result;
 };
 
 // Do not edit below this line
