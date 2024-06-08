55c206e89fd2b0ad8864880698a9eafc2eef2d1c
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-08 14:24:18 +0300

    Finish 02 repeat string task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else if (text === '' || num === 0) {
+        return '';
+    }
+    else {
+        return text.repeat(num);
+    }
 };
 
 // Do not edit below this line
