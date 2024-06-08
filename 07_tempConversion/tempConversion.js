446f107f9f78f46b9f679f0b70aed9563d436cd1
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-08 17:11:44 +0300

    Finish 07 temperature conversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let celsius = (temperature - 32) * 5 / 9;
+  let roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let fahrenheit = temperature * 9 / 5 + 32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
